cca6ac566eb38c14829e09dea866a0ecb9437abb
Aviral Khattar <aviral92@gmail.com>
2018-02-03 21:45:34 -0500

    Create InsertPosition.java

---
+++ InsertPosition.java
@@ -0,0 +1,17 @@
+/*
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+*/
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        for(int i =0; i<nums.length; i++){
+            if(nums[i]==target)
+                return i;
+            else if (nums[i]>target)
+                return i--;
+        }
+        return nums.length;
+    }
+}
